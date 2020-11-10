81ae4774816496ea038939f0b7f4ba46150ccd41
alexgausman <gausman.a@gmail.com>
2020-11-10 15:24:52 -0800

    Sync

---
+++ contracts/ITokenManager.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.8;
+
+interface ITokenManager {
+    function assignVested(
+        address _receiver,
+        uint256 _amount,
+        uint64 _start,
+        uint64 _cliff,
+        uint64 _vested,
+        bool _revokable
+    )
+        external
+        returns (uint256);
+}
--- contracts/XSale.sol
+++ contracts/XSale.sol
@@ -3,18 +3,17 @@
 pragma solidity 0.6.8;
 
 import "./SafeMath.sol";
-import "./Ownable.sol";
+import "./Pausable.sol";
 import "./INFTX.sol";
 import "./IXStore.sol";
 import "./IERC721.sol";
+import "./ITokenManager.sol";
 import "./Context.sol";
 import "./ReentrancyGuard.sol";
 
-contract XSale is Ownable {
+contract XSale is Pausable, ReentrancyGuard {
     using SafeMath for uint256;
     using EnumerableSet for EnumerableSet.UintSet;
-
-    // bool public isPaused;
 
     address public nftxAddress;
     address public nftxTokenAddress;
@@ -22,80 +21,58 @@ contract XSale is Ownable {
     INFTX public nftx;
     IXStore public xStore;
     IERC20 public nftxToken;
+    ITokenManager public tokenManager;
 
-    mapping(uint256 => NftBounty) public nftReqs;
+    mapping(uint256 => Bounty[]) public bounties;
 
-    mapping(uint256 => NftBounty[]) public nftBounties;
-
-    struct NftBounty {
-        uint256 priceInNftx;
-        uint256 quantity;
+    struct Bounty {
+        uint256 reward;
+        uint256 request;
     }
 
-    constructor(address _nftxAddress, address _nftxTokenAddress) public {
+    constructor(
+        address _nftxAddress, 
+        address _nftxTokenAddress,
+        address tokenManagerAddress
+    ) public {
+        initOwnable();
         nftxAddress = _nftxAddress;
         nftxTokenAddress = _nftxTokenAddress;
         nftx = INFTX(nftxAddress);
         xStore = IXStore(nftx.storeAddress());
         nftxToken = IERC20(nftxTokenAddress);
+        tokenManager = ITokenManager(tokenManagerAddress);
     }
 
-    // function setIsPaused(bool _isPaused) public onlyOwner {
-    //     isPaused = _isPaused;
-    // }
-
-    function _addBounty(uint256 vaultId, uint256 nftxPrice, uint256 quantity)
-        internal
+    function addBounty(uint256 vaultId, uint256 reward, uint256 request)
+        public onlyOwner
     {
-        NftBounty memory newNftBounty;
-        newNftBounty.priceInNftx = nftxPrice;
-        newNftBounty.quantity = quantity;
-        nftBounties[vaultId].push(newNftBounty);
+        Bounty memory newBounty;
+        newBounty.reward = reward;
+        newBounty.request = request;
+        bounties[vaultId].push(newBounty);
     }
 
-    function _setBounty(
+    function setBounty(
         uint256 vaultId,
         uint256 bountyIndex,
-        uint256 nftxPrice,
-        uint256 quantity
-    ) internal {
-        NftBounty storage nftBounty = nftBounties[vaultId][bountyIndex];
-        nftBounty.priceInNftx = nftxPrice;
-        nftBounty.quantity = quantity;
+        uint256 newReward,
+        uint256 newRequest
+    ) public onlyOwner {
+        Bounty storage bounty = bounties[vaultId][bountyIndex];
+        bounty.reward = newReward;
+        bounty.request = newRequest;
     }
 
-    function fillBountyUsingNFTs(
-        uint256 vaultId,
-        uint256 bountyIndex,
-        uint256[] memory nftIds
-    ) public nonReentrant {
-        
-        uint256 acc;
-        NftBounty storage nftBounty = nftBounties[vaultId][bountyIndex];
-        for (uint256 i = 0; i < nftIds.length; i = i.add(1)) {
-            if (
-                !nftx.isEligible(vaultId, nftIds[i]) ||
-                nftBounty.quantity < 1
-            ) {
-                break;
-            }
-            xStore.nft(vaultId).safeTransferFrom(
-                _msgSender(),
-                nftxAddress,
-                nftIds[i]
-            );
-            nftBounty.quantity = nftBounty.quantity.sub(1);
-            acc = acc.add(nftBounty.priceInNftx);
-        }
-        // TODO: transfer vested NFTX tokens
-    }
-
-    function fillBountyUsingXToken(uint256 vaultId, uint256 bountyIndex, uint256 amount) public nonReentrant {
-        NftBounty storage nftBounty = nftBounties[vaultId][bountyIndex];
-        uint256 _amount = nftBounty.quantity < amount ? nftBounty.quantity : amount;
+    function fillBounty(uint256 vaultId, uint256 bountyIndex, uint256 amount) public nonReentrant {
+        Bounty storage bounty = bounties[vaultId][bountyIndex];
+        uint256 _amount = bounty.request < amount ? bounty.request : amount;
         if (_amount > 0) {
             xStore.xToken(vaultId).transferFrom(_msgSender(), nftxAddress, _amount);
-            nftBounty.quantity = nftBounty.quantity.sub()
+            uint256 _reward = bounty.reward.mul(_amount).div(bounty.request);
+            bounty.request = bounty.request.sub(_amount);
+            bounty.reward = bounty.reward.sub(_reward);
+            // TODO: tokenManager.assignVested(...);
         }
     }
 }
