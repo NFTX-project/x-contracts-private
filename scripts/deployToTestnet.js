49dc63843bcae72ce9de8a93961f2abc20abd883
Alex Gausman <gausman.a@gmail.com>
2020-11-29 23:25:01 -0800

    Sync

--- scripts/deployToTestnet.js
+++ scripts/deployToTestnet.js
@@ -2,7 +2,9 @@
 
 const { ethers, upgrades } = require("@nomiclabs/buidler");
 
-const zeroAddress = "0x0000000000000000000000000000000000000000";
+// const zeroAddress = "0x0000000000000000000000000000000000000000";
+
+const rinkebyDaoAddress = "0xeddb1b92b9ad55a5bb1dcc22c23e7839cd3dc99c";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -17,26 +19,50 @@ async function main() {
   /* const Cpm = await ethers.getContractFactory("CryptoPunksMarket");
   const cpm = await Cpm.deploy();
   await cpm.deployed();
-  console.log("CPM address:", cpm.address);
+  console.log("CPM address:", cpm.address); */
   const XStore = await ethers.getContractFactory("XStore");
   const xStore = await XStore.deploy();
   await xStore.deployed();
-  console.log("XStore address:", xStore.address); */
+  console.log("XStore address:", xStore.address);
+
   const Nftx = await ethers.getContractFactory("NFTX");
-  let nftx = await upgrades.deployProxy(Nftx, [zeroAddress, zeroAddress], {
+  let nftx = await upgrades.deployProxy(Nftx, [xStore.address], {
     initializer: "initialize",
   });
   await nftx.deployed();
-  console.log("NFTX address:", nftx.address);
+  console.log("NFTX proxy address:", nftx.address);
 
-  const NftxV2 = await ethers.getContractFactory("NFTXv2");
+  const ProxyController = await ethers.getContractFactory("ProxyController");
+  const proxyController = await ProxyController.deploy(nftx.address);
+  await proxyController.deployed();
+  console.log("ProxyController address:", proxyController.address);
+
+  // let proxyAdminAddr = await proxyController.getAdmin();
+  // console.log("Proxy admin:", proxyAdminAddr);
+
+  await upgrades.admin.changeProxyAdmin(nftx.address, proxyController.address);
+  console.log("Updated NFTX proxy admin");
+
+  // proxyAdminAddr = await proxyController.getAdmin();
+  // console.log("Proxy admin:", proxyAdminAddr);
+
+  /* await proxyController.updateImplAddress();
+  const implAddress = await proxyController.implAddress();
+  console.log("NFTX implementation address:", implAddress); */
+
+  /* const NftxV2 = await ethers.getContractFactory("NFTXv2");
   console.log("Preparing upgrade...");
   const nftxV2Address = await upgrades.prepareUpgrade(nftx.address, NftxV2);
-  console.log("NftxV2 at:", nftxV2Address);
+  console.log("NftxV2 at:", nftxV2Address); */
 
-  // nftx.upgradeTo( )
+  await proxyController.transferOwnership(rinkebyDaoAddress);
+  console.log("Updated ProxyController owner");
 
-  // await xStore.transferOwnership(nftx.address);
+  await xStore.transferOwnership(nftx.address);
+  console.log("Updated XStore owner");
+
+  await nftx.transferOwnership(rinkebyDaoAddress);
+  console.log("Updated NFTX owner");
 }
 
 main()
