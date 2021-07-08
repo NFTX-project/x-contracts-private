3155a7d2f1309dbee9b4855353a385cddb334694
Alex Gausman <alexgausman@Alexs-MacBook-Pro.local>
2021-07-08 10:25:14 -0700

    Update repo

--- scripts/mainnet/deployNewImpl.js
+++ scripts/mainnet/deployNewImpl.js
@@ -11,18 +11,19 @@ async function main() {
 
   /* const xStore = await ethers.getContractAt("XStore", addresses.xStore);
   const nftx = await ethers.getContractAt("NFTX", addresses.nftx); */
+
   const proxyCont = await ethers.getContractAt(
     "ProxyController",
     addresses.proxyController
   );
 
-  const NFTXv7 = await ethers.getContractFactory("NFTXv7");
+  const NFTXv11 = await ethers.getContractFactory("NFTXv11");
 
   // const nftxv7Address = await upgrades.prepareUpgrade(addresses.nftx, NFTXv7);
 
-  const nftxv7 = await NFTXv7.deploy();
+  const nftxv11 = await NFTXv11.deploy();
 
-  console.log("NFTXv7 Implementation:", nftxv7.address);
+  console.log("NFTXv11 Implementation:", nftxv11.address);
 
   console.log(
     "\nNow go and call proxyController.upgradeProxyTo(...) from Aragon Agent\n"
---
+++ scripts/rinkeby/deployXTokenFactory.js
@@ -0,0 +1,41 @@
+const { ethers, upgrades } = require("@nomiclabs/buidler");
+
+const addresses = require("../../addresses/rinkeby.json");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const XTokenClonable = await ethers.getContractFactory("XTokenClonable");
+  const xTokenClonable = await XTokenClonable.deploy();
+  
+  await xTokenClonable.deployed();
+  console.log('xtokenclonable deployed at', xTokenClonable.address);
+  await xTokenClonable.initialize('Template Token', 'TEMPLATE', {
+    gasLimit: "9500000",
+  });
+  console.log('xtokenclonable initialized');
+  
+
+  const XTokenFactory = await ethers.getContractFactory("XTokenFactory");
+  const xTokenFactory = await XTokenFactory.deploy(xTokenClonable.address);
+  await xTokenFactory.deployed();
+  console.log('xtokenfactory deployed at', xTokenFactory.address);
+  await xTokenFactory.transferOwnership(addresses.nftx); 
+  
+
+  console.log("-- DONE --");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
